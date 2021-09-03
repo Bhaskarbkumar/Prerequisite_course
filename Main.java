d61c087aca930cd5012a5c6963e87bb7a84a6cd3
bhaskarbkumar <bhaskarbkumar@gmail.com>
2021-09-02 23:38:49 -0500

    Final commit

--- Main.java
+++ Main.java
@@ -1,12 +1,31 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Container;
+import javax.swing.JPanel;
+
 
 public class Main{
 
     public static void main (String[] args){
 
         JFrame window =new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
          window.setSize(600,200);
          window.setLocation(400, 200);
+
+         Container cp=window.getContentPane();
+         JPanel panel=new JPanel();
+
+         panel.add(new JTextField("Hello World"));
+         panel.add(new JButton("CLICK ON ME"));
+
+         panel.add(new JButton("Sign Up"));
+         panel.add(new JTextField("Java World"));
+         cp.add(panel);
+
          window.pack();
          window.setVisible(true);
 
